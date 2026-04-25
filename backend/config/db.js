42d19c64e74d889417222809f43500bcccc6cd62
Ashutosh Kumar <smurfstack.dev@gmail.com>
2026-04-25 20:49:07 +0530

    updated the dotenv file

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
